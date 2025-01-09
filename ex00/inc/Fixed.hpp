f9be8530c96fdefb72b018eb946158d5b33dac12
omartela <Oskari Martela>
2025-01-09 10:52:56 +0200

    Adds exercise00

---
+++ ex00/inc/Fixed.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omartela <omartela@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/09 07:13:32 by omartela          #+#    #+#             */
+/*   Updated: 2025/01/09 07:13:34 by omartela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <string>
+
+class Fixed
+{
+    private:
+        int _value;
+        static const int _fractionalbits = 8;
+    public:
+        Fixed();
+        ~Fixed();
+        Fixed(const Fixed &other);
+        Fixed   &operator=(const Fixed &other);
+        int     getRawBits(void) const;
+        void    setRawBits(int const raw);
+};
---
+++ ex00/src/Fixed.cpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omartela <omartela@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/09 09:05:59 by omartela          #+#    #+#             */
+/*   Updated: 2025/01/09 09:06:00 by omartela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed() : _value(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    this->operator=(other);
+}
+
+Fixed &Fixed::operator=(const Fixed &other)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &other)
+    {
+        _value = other.getRawBits();
+    }
+    return (*this);
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return (_value);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    std::cout << "setRawBits member function called" << std::endl;
+    _value = raw;
+}
